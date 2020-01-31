94e709549135bb03534b29048512795b6004cab3
Himanshu Panwar <himanshu.panwar@nagarro.com>
2020-01-31 20:50:24 +0530

    endpoint

--- src/main/java/assesment/controller/PaymentController.java
+++ src/main/java/assesment/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package assesment.controller;
 
-import java.math.BigDecimal;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +29,7 @@ public class PaymentController {
 	@Autowired
 	PaymentService service;
 	
-	@PostMapping("/check")
+	@PostMapping("/getNetPay")
 	public ResponseEntity<Object> getNetPayment(@RequestBody @Valid PaymentDto payInfo, BindingResult result) {
 		
 		PaymentResponse response = new PaymentResponse();
@@ -43,94 +41,6 @@ public class PaymentController {
 		response = service.getNetPayment(payInfo);
 		return new ResponseEntity<Object>(response, HttpStatus.OK);
 		
-	}
-
-
-	@PostMapping("/netPay")
-	public ResponseEntity<Object> getNetPayment(@Valid PaymentDto payInfo){
-
-		String payableRequestInit = payInfo.getPayment();
-		String userStatus = payInfo.getUserStatus();
-		String isItemGrocery = payInfo.getIsGrocery();
-
-		try {
-			String payableRequest[] = payableRequestInit.split("\\$");
-			String payable = payableRequest[1];
-			BigDecimal netPayable = null;
-			BigDecimal tempPay = new BigDecimal(payable);
-			
-			if(isItemGrocery.equals("T")) {
-				if(tempPay.intValue()==100 || tempPay.intValue()>100) {
-					netPayable = tempPay.subtract(tempPay.multiply(BigDecimal.valueOf(0.05)));
-					String finalAmount =  utility.getCurrencyFormatter(netPayable.doubleValue());
-					return new ResponseEntity<Object>(finalAmount, HttpStatus.OK);
-				}
-				return new ResponseEntity<Object>(payableRequestInit, HttpStatus.OK);
-			}
-			else {
-
-				if(userStatus.equals("isEmp")) {
-					if(tempPay.intValue()>100) {
-						netPayable = tempPay.subtract(tempPay.multiply(BigDecimal.valueOf(0.05)));	
-						tempPay = netPayable;
-					}
-					int percentage = 30;
-					netPayable = tempPay.multiply(BigDecimal.valueOf((double)percentage/100));
-					String finalAmount =  utility.getCurrencyFormatter(netPayable.doubleValue());
-					return new ResponseEntity<Object>(finalAmount, HttpStatus.OK);
-				}
-				else if(userStatus.equals("isAffl")) {
-					if(tempPay.intValue()>100) {
-						netPayable = tempPay.subtract(tempPay.multiply(BigDecimal.valueOf(0.05)));	
-						tempPay = netPayable;
-					}
-					int percentage = 10;
-					netPayable = tempPay.multiply(BigDecimal.valueOf((double)percentage/100));
-					String finalAmount =  utility.getCurrencyFormatter(netPayable.doubleValue());
-					return new ResponseEntity<Object>(finalAmount, HttpStatus.OK);
-
-				}
-				else if(userStatus.equals("isCstmrYr2")) {
-					if(tempPay.intValue()>100) {
-						netPayable = tempPay.subtract(tempPay.multiply(BigDecimal.valueOf(0.05)));	
-						tempPay = netPayable;
-					}
-					int percentage = 5;
-					netPayable = tempPay.multiply(BigDecimal.valueOf((double)percentage/100));
-					String finalAmount =  utility.getCurrencyFormatter(netPayable.doubleValue());
-					return new ResponseEntity<Object>(finalAmount, HttpStatus.OK);
-
-				}
-				else {
-					return new ResponseEntity<Object>(payableRequestInit, HttpStatus.OK);
-
-				}		
-			}
-		}
-		catch(Exception ex) {
-			System.out.println("dsds");
-		}
-		return new ResponseEntity<Object>(payableRequestInit, HttpStatus.OK);
-
-	}
-
-	public static void main(String args[]) {
-
-		String amount ="430.543322";
-		BigDecimal amounts = new BigDecimal(amount);
-		int percentage = 50;
-		BigDecimal percentageAmount = amounts.multiply(BigDecimal.valueOf((double)percentage/100));
-		double db = percentageAmount.doubleValue();
-		System.out.println(db);
-
-		String as = "350.99";
-		BigDecimal am = new BigDecimal(as);
-		if(am.intValue()>100) {
-			BigDecimal percentageAmounts = am.subtract(am.multiply(BigDecimal.valueOf(0.05)));	
-
-			System.out.println(percentageAmounts.toString());
-		}
-
 	}
 
 }
